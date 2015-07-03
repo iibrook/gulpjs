af5afa88306550f7909d55b1afeb2b2488504a5a
epirus <lichunyi@lichunyi-2.local>
2015-07-03 11:16:33 +0800

    add the runsequence

--- tasks/release.js
+++ tasks/release.js
@@ -1,3 +1,9 @@
 var gulp = require('gulp');
 var dir = require('./directory.js');
+var runSequence = require('gulp-run-sequence');
+
 gulp.task("release",['imagemin','usemin','reveasy'])
+
+gulp.task('release', function(cb) {
+  runSequence('imagemin','usemin', 'reveasy');
+});
